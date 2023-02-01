d9be136b66f946253ee12bb9f0a68f5daf7e73c6
Bryan W <bhoff1980@gmail.com>
2023-02-01 05:12:07 -0700

    basic setup of react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomeScreen from './Screens/HomeScreen';
-import {Container} from 'react-bootstrap'
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./Screens/HomeScreen";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route  } from "react-router-dom";
+import ProductScreen from "./Screens/ProductScreen";
 
 function App() {
   return (
-    <>
-    <Header/>
-    <main className='py-3'>
-      <Container>
-   <HomeScreen/>
-   </Container>
-   </main>
-   <Footer/>
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path='/' component={HomeScreen}exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,45 @@
-import React from 'react'
-import {Container, Row, Col, Navbar, Nav, NavDropdown, Form, Button, FormControl} from 'react-bootstrap'
+import React from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  Button,
+  FormControl,
+} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 const Header = () => {
   return (
     <>
       <header>
-      <Navbar bg="light" expand="lg" collapseOnSelect>
-        <Container>
-  <Navbar.Brand href="/">EarthShip Packaging</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="ml-auto margin">
-      <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-      <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-  </Container>
-</Navbar>
+        <Navbar bg="light" expand="lg" collapseOnSelect>
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>EarthShip Packaging</Navbar.Brand>
+            </LinkContainer>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto margin">
+              <LinkContainer to="/cart">
+                <Nav.Link >
+                  <i className="fas fa-shopping-cart"></i> Cart
+                </Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                <Nav.Link >
+                  <i className="fas fa-user"></i> Sign In
+                </Nav.Link>
+                </LinkContainer>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
