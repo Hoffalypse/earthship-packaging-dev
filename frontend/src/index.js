71503046f19a170d847ef76517bfbd8c4d96efdb
Bryan W <bhoff1980@gmail.com>
2023-02-02 04:58:23 -0700

    setup redux and started to setup products in redux, constants, reducers, actions

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {Provider, provider} from 'react-redux'
+import store from './store'
 import 'bootswatch/dist/slate/bootstrap.min.css';
 import './index.css';
 
@@ -9,9 +11,11 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store = {store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </Provider>
 );
 
 
