ebbce2c29f46a75f6f552f5a936f3f84c2d4e4e9
Bryan W <bhoff1980@gmail.com>
2023-02-05 18:27:24 -0700

    working through redux logic

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -5,7 +5,7 @@ import {
   PRODUCT_LIST_FAIL,
 } from "../constants/productConstants";
 
-
+// redux actions
 export const listProducts = () => async (dispatch) => {
     try{
         dispatch ({type: PRODUCT_LIST_REQUEST})
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,5 +1,7 @@
 import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL} from '../constants/productConstants'
 
+
+//redux reducers
 export const productListReducer = (state= {products: []}, action ) =>{
 switch(action.type ) {
     case PRODUCT_LIST_REQUEST:
