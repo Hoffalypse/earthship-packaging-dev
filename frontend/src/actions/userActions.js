384cb09a167e1ed5a76cfbba9e0a4f72ccda9b30
Bryan W <bhoff1980@gmail.com>
2023-02-14 02:02:45 -0700

    started building update profile page, some limited bugs to work out, profile is logging out for some reason

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,12 +1,15 @@
 import axios from "axios";
 import {
-    USER_LOGIN_FAIL,
+  USER_DETAILS_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
   USER_REGISTER_FAIL,
   USER_REGISTER_REQUEST,
-  USER_REGISTER_SUCCESS
+  USER_REGISTER_SUCCESS,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -16,7 +19,7 @@ export const login = (email, password) => async (dispatch) => {
     });
     const config = {
       headers: {
-        "Conatent-Type": "application/json",
+        "Content-Type": "application/json",
       },
     };
     const { data } = await axios.post(
@@ -30,19 +33,22 @@ export const login = (email, password) => async (dispatch) => {
       payload: data,
     });
 
-    localStorage.setItem('userInfo',JSON.stringify(data) )
+    localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
-        type: USER_LOGIN_FAIL,
-        payload: error.response && error.response.data.message ? error.response.data.message : error.message
-    })
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
 
-export const logout  = () => (dispatch) => {
-  localStorage.removeItem('userInfo')
-  dispatch({type: USER_LOGOUT})
-}
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
+};
 
 export const register = (name, email, password) => async (dispatch) => {
   try {
@@ -51,7 +57,7 @@ export const register = (name, email, password) => async (dispatch) => {
     });
     const config = {
       headers: {
-        "Conatent-Type": "application/json",
+        "Content-Type": "application/json",
       },
     };
     const { data } = await axios.post(
@@ -70,11 +76,49 @@ export const register = (name, email, password) => async (dispatch) => {
       payload: data,
     });
 
-    localStorage.setItem('userInfo',JSON.stringify(data) )
+    localStorage.setItem("userInfo", JSON.stringify(data));
   } catch (error) {
     dispatch({
-        type: USER_REGISTER_FAIL,
-        payload: error.response && error.response.data.message ? error.response.data.message : error.message
-    })
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+export const getUserDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: USER_DETAILS_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+    const { data } = await axios.get(
+      `/api/users/${id}`,
+
+      config
+    );
+
+    dispatch({
+      type: USER_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: USER_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
