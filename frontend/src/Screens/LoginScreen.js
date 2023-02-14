dcff0c811946e28473b4398900c45981373892ec
Bryan W <bhoff1980@gmail.com>
2023-02-13 22:42:51 -0700

    user login page has complete functionality with redux

--- frontend/src/Screens/LoginScreen.js
+++ frontend/src/Screens/LoginScreen.js
@@ -7,21 +7,35 @@ import Loader from "../components/Message.js";
 import { login } from "../actions/userActions";
 import FormContainer from "../components/FormContainer.js";
 
-const LoginScreen = ({location}) => {
+const LoginScreen = ({location, history}) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const dispatch = useDispatch();
+  const userLogin = useSelector(state => state.userLogin)
+  const { loading, error, userInfo} = userLogin
+
   const redirect = location.search ? location.search.split('=')[1] : '/'
+
+  useEffect(() => {
+    if(userInfo){
+        history.push(redirect)
+    }
+  },[history, userInfo, redirect])
+  
 
   const submitHandler = (e) => {
     e.preventDefault();
     //This is where I will put the dispatch login data 
+    dispatch(login(email, password))
 
   }
 
   return (
     <FormContainer>
       <h1>Sign In</h1>
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader/>}
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="email">
           <Form.Label> Email Address</Form.Label>
@@ -49,7 +63,9 @@ const LoginScreen = ({location}) => {
       </Form>
       <Row className="py-3">
         <Col>
-        New Customer? <Link to={redirect ? `/register?redirect=${redirect}`: '/register'}></Link>
+        New Customer? <Link to={redirect ? `/register?redirect=${redirect}`: '/register'}>
+            Register Here
+        </Link>
         </Col>
       </Row>
     </FormContainer>
