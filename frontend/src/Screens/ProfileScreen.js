db1d23d3319b1949c900558b01a9bc3ac5cb8c53
Bryan W <bhoff1980@gmail.com>
2023-02-22 19:27:38 -0700

    added update functionality to profile screen

--- frontend/src/Screens/ProfileScreen.js
+++ frontend/src/Screens/ProfileScreen.js
@@ -3,7 +3,8 @@ import { Form, Button, Row, Col } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message.js";
 import Loader from "../components/Message.js";
-import { getUserDetails } from "../actions/userActions";
+import { getUserDetails, updateUserProfile } from "../actions/userActions";
+import {USER_UPDATE_PROFILE_RESET} from '../constants/userConstants'
 
 
 const ProfileScreen = ({ location, history }) => {
@@ -20,6 +21,9 @@ const ProfileScreen = ({ location, history }) => {
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
+  const userUpdateProfile = useSelector((state) => state.userUpdateProfile)
+  const { success } = userUpdateProfile
+
 
 
   useEffect(() => {
@@ -27,14 +31,15 @@ const ProfileScreen = ({ location, history }) => {
       history.push('/login');
     }
     else {
-            if(!user.name){
+            if(!user.name || success){
+              dispatch({type: USER_UPDATE_PROFILE_RESET})
                 dispatch(getUserDetails('profile'))
             }else{
                 setName(user.name)
                 setEmail(user.email)
             }
     }
-  }, [dispatch, history, userInfo, user]);
+  }, [dispatch, history, userInfo, user, success]);
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -42,7 +47,7 @@ const ProfileScreen = ({ location, history }) => {
     if (password !== confirmPassword) {
       setMessage("Passwords do not Match");
     } else {
-     
+     dispatch(updateUserProfile({id:user._id, name, email, password}))
     }
   };
 
@@ -52,6 +57,7 @@ const ProfileScreen = ({ location, history }) => {
         <h2>User Profile</h2>
       {message && <Message variant="danger">{message}</Message>}
       {error && <Message variant="danger">{error}</Message>}
+      {success && <Message variant="success">Profile Updated</Message>}
       {loading && <Loader />}
       <Form onSubmit={submitHandler}>
             <Form.Group controlId='name'>
