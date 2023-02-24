bc4c7e9638766fc040c258d604b8c57e172380b6
Bryan W <bhoff1980@gmail.com>
2023-02-23 22:33:57 -0700

    working through shipping page

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -26,8 +26,14 @@ const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartInfo"))
   : null;
 
+  const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: { cartItems: cartItemsFromStorage, 
+    shippingAddress: shippingAddressFromStorage, 
+  },
   userLogin:{userInfo: userInfoFromStorage}
 };
 const middleware = [thunk];
