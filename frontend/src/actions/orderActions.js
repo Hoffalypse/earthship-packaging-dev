557f5d3ed98ae2d023dd4e71e490a0aa37cf7aed
Bryan W <bhoff1980@gmail.com>
2023-02-27 01:12:08 -0700

    redux setup for paypal implementation

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -1,4 +1,4 @@
-import {ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL, ORDER_DETAILS_FAIL, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_REQUEST} from '..//constants/orderConstants'
+import {ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL, ORDER_DETAILS_FAIL, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_REQUEST, ORDER_PAY_FAIL, ORDER_PAY_SUCCESS, ORDER_PAY_REQUEST} from '..//constants/orderConstants'
 import axios from 'axios'
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -78,6 +78,46 @@ export const createOrder = (order) => async (dispatch, getState) => {
       }
       dispatch({
         type: ORDER_DETAILS_FAIL,
+        payload: message,
+      })
+    }
+  }
+
+  export const payOrder = (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_PAY_REQUEST,
+      })
+  
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.put(`/api/orders/${orderId}/pay`, paymentResult, config)
+  
+      dispatch({
+        type: ORDER_PAY_SUCCESS,
+        payload: data,
+      })
+
+      // localStorage.removeItem('cartItems')
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        // dispatch(logout())
+      }
+      dispatch({
+        type: ORDER_PAY_FAIL,
         payload: message,
       })
     }
