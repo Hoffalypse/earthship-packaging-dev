904043a4d58d4f4139262f86a86546fe83a0b313
Bryan W <bhoff1980@gmail.com>
2023-02-28 00:44:17 -0700

    setup admin user edit functionality on the backend

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -42,15 +42,16 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // update user profile ----private api/user/profile
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id)
 
   if (user) {
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
     if (req.body.password) {
-      user.password = req.body.password;
+      user.password = req.body.password
     }
-    const updatedUser = await user.save();
+
+    const updatedUser = await user.save()
 
     res.json({
       _id: updatedUser._id,
@@ -58,12 +59,12 @@ const updateUserProfile = asyncHandler(async (req, res) => {
       email: updatedUser.email,
       isAdmin: updatedUser.isAdmin,
       token: generateToken(updatedUser._id),
-    });
+    })
   } else {
-    res.status(404);
-    throw new Error("User not Found");
+    res.status(404)
+    throw new Error('User not found')
   }
-});
+})
 
 // register new user ---Public
 
@@ -117,4 +118,39 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser };
+//get a user for editing --private/admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+  if(user){
+  res.json(user);
+  }else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
+});
+
+
+//admin edit user --private/admin
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    user.isAdmin = req.body.isAdmin
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers, deleteUser, getUserById, updateUser };
