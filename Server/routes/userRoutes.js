6d368ed21a87c013491ba1faeb88d3de368bde87
Bryan W <bhoff1980@gmail.com>
2023-02-27 23:11:22 -0700

    setup middleware function to check isAdmin

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -1,14 +1,14 @@
 import express from 'express'
 const router = express.Router()
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 
 // This is the api/users route
 
 
 // Post to login
-router.route('/', registerUser).post(registerUser)
+router.route('/', registerUser).post(registerUser).get(protect, admin, getUsers)
 router.post("/login",authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
