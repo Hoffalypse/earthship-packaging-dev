c0a81f9d1322d9be8b9593b4e333751df508733c
Bryan W <bhoff1980@gmail.com>
2023-02-28 00:31:19 -0700

    delete finctionality fully working

--- frontend/src/Screens/UserListScreen.js
+++ frontend/src/Screens/UserListScreen.js
@@ -15,8 +15,8 @@ const UserListScreen = ({ history }) => {
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
 
-//   const userDelete = useSelector((state) => state.userDelete)
-//   const { success: successDelete } = userDelete
+  const userDelete = useSelector((state) => state.userDelete)
+  const { success: successDelete } = userDelete
 
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
@@ -24,12 +24,12 @@ const UserListScreen = ({ history }) => {
     } else {
       history.push('/login')
     }
-  }, [dispatch, history, userInfo])
+  }, [dispatch, history, successDelete, userInfo])
 
   const deleteHandler = (id) => {
-    // if (window.confirm('Are you sure')) {
-    //   dispatch(deleteUser(id))
-    // }
+    if (window.confirm('Are you sure you want to delete?')) {
+      dispatch(deleteUser(id))
+    }
   }
 
   return (
