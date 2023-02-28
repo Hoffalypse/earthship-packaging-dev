0465a8b801cc1dbf8c3c6c4db0268f3295b29488
Bryan W <bhoff1980@gmail.com>
2023-02-27 21:23:10 -0700

    profile page shows past orders

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Provider, provider} from 'react-redux'
+import {Provider} from 'react-redux'
 import store from './store'
 import 'bootswatch/dist/slate/bootstrap.min.css';
 import './index.css';
