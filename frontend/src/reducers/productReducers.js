e38259b3703f42a090cbcdb3fb9c11f7554cb04e
Bryan W <bhoff1980@gmail.com>
2023-03-01 01:24:39 -0700

    product edit screen has all functionality besides image upload

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -12,6 +12,10 @@ import {
   PRODUCT_CREATE_SUCCESS,
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_RESET,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
+  PRODUCT_UPDATE_RESET,
 } from "../constants/productConstants";
 
 //redux reducers
@@ -55,7 +59,7 @@ export const productDeleteReducer = (state = {}, action) => {
       return { loading: true, ...state };
 
     case PRODUCT_DELETE_SUCCESS:
-      return { loading: false, success:true };
+      return { loading: false, success: true };
 
     case PRODUCT_DELETE_FAIL:
       return { loading: false, error: action.payload };
@@ -71,15 +75,30 @@ export const productCreateReducer = (state = {}, action) => {
       return { loading: true, ...state };
 
     case PRODUCT_CREATE_SUCCESS:
-      return { loading: false, success:true, product: action.payload};
+      return { loading: false, success: true, product: action.payload };
 
     case PRODUCT_CREATE_FAIL:
       return { loading: false, error: action.payload };
 
-      case PRODUCT_CREATE_RESET:
-        return{}
+    case PRODUCT_CREATE_RESET:
+      return {};
 
     default:
       return state;
   }
 };
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCT_UPDATE_REQUEST:
+      return { loading: true }
+    case PRODUCT_UPDATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload }
+    case PRODUCT_UPDATE_FAIL:
+      return { loading: false, error: action.payload }
+    case PRODUCT_UPDATE_RESET:
+      return { product: {} }
+    default:
+      return state
+  }
+}
