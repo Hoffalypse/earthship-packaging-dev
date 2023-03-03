50a7db7edb5ea76d7ec0ece8502da8d9b350b310
Bryan W <bhoff1980@gmail.com>
2023-03-03 01:14:17 -0700

    server.js setup for production

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,15 @@
-import path from 'path'
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
-import morgan from 'morgan';
+import morgan from "morgan";
 import connectDB from "./config/db.js";
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
-
 
 dotenv.config();
 
@@ -18,27 +17,37 @@ connectDB();
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-app.get("/", (req, res) => {
-  res.send("API is running like the wind ");
-});
+
 
 app.use("/api/products", productRoutes);
-app.use('/api/users', userRoutes)
-app.use('/api/orders', orderRoutes)
-app.use('/api/upload', uploadRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
-const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.use(notFound) 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get('*', (re, res) => res.sendFile(path.resolve(__dirname, 'frontend, build', 'index.html')))
+}else{
+  app.get("/", (req, res) => {
+    res.send("API is running like the wind ");
+  });
+}
+
+app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
